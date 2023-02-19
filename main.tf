54d99e7f5b9c06e6cac7cbace01396995b33a529
lolitaad989 <lolitaad19@gmail.com>
2023-02-19 21:30:46 +0530

    tf basics

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-module "vpc" {
-  source = "git::https://github.com/lolitaad1989/tf-module-vpc.git"
-}
+# module "vpc" {
+#   source = "git::https://github.com/lolitaad1989/tf-module-vpc.git"
+# }
 
 #b default it clones from main branch
 #we cannot parameterize on module souces
