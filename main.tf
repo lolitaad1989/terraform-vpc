0c135a8c91ca64a184e9034b855d50b422c0eca8
lolitaad989 <lolitaad19@gmail.com>
2023-02-21 19:05:31 +0530

    tf push

--- main.tf
+++ main.tf
@@ -6,13 +6,17 @@
 #we cannot parameterize on module souces
 
 module "vpc" {
-  source = "./vendor/modules/vpc"
-  VPC_CIDR = var.VPC_CIDR
-  ENV = var.ENV
-  AZ= var.AZ
-  PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-  
+  source               = "./vendor/modules/vpc"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+  VPC_CIDR             = var.VPC_CIDR
+  ENV                  =  var.ENV
+  AZ                   = var.AZ   
+  PUBLIC_SUBNET_CIDR   = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR  = var.PRIVATE_SUBNET_CIDR
+  DEFAULT_VPC_CIDR     = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_ID       = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_RT       = var.DEFAULT_VPC_RT
+
+
 }
 
 
--- terraform.auto.tfvars
+++ terraform.auto.tfvars
@@ -1,1 +1,4 @@
 AZ = ["us-east-1a", "us-east-1b"]
+DEFAULT_VPC_CIDR = "172.31.0.0/16"
+DEFAULT_VPC_ID = "vpc-0ef020a67b03b639a"
+DEFAULT_VPC_RT = "rtb-06542cb77a02581d2"
